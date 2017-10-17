e248fb932a744e2b11fd7d5b397ad649e7c48922
Sanyam Khurana <Sanyam@SanyamKhurana.com>
2017-10-18 00:23:33 +0530

    feat(setup_mac.sh): Adds custom ZSH theme from Oh My ZSH

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -60,6 +60,7 @@ brew install jq         # https://stedolan.github.io/jq/
 brew install python3
 brew install editorconfig
 brew install ssh-copy-id  # http://linux.die.net/man/1/ssh-copy-id
+brew install elixir
 
 # Native apps
 brew tap phinze/homebrew-cask
@@ -71,6 +72,52 @@ function installcask() {
         echo "${@} is already installed."
     fi
 }
+
+install_oh_my_zsh () {
+    # Test to see if zshell is installed.  If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Install Oh My Zsh if it isn't already present
+        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+            sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+        fi
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
+        fi
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname);
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            if [[ -f /etc/redhat-release ]]; then
+                sudo yum install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/debian_version ]]; then
+                sudo apt-get install zsh
+                install_zsh
+            fi
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "We'll install zsh, then re-run this script!"
+            brew install zsh
+            exit
+        fi
+    fi
+}
+
+install_oh_my_zsh
+
+###############################################################################
+# Zsh                                                                         #
+###############################################################################
+
+# Install Zsh settings
+ln -s $PWD/zsh/themes/curiouslearner.zsh-theme $HOME/.oh-my-zsh/themes
+
+###############################################################################
+# Install utilities                                                           #
+###############################################################################
 
 installcask android-file-transfer
 installcask google-chrome
@@ -93,6 +140,11 @@ installcask pgadmin3
 # launchctl load ~/Library/LaunchAgents/homebrew.mxcl.postgresql.plist
 psql postgres -c 'CREATE EXTENSION "adminpack";'
 sudo gem install pg
+
+# PG tools needed for every other project:
+brew tap osgeo/osgeo4mac
+brew install gdal
+brew install postgis
 
 # Fonts
 brew tap caskroom/fonts
