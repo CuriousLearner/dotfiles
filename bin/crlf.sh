5d39e05cd2444457f660334f7076a9f3e69cbf20
storymode7 <17mayanksinghal@gmail.com>
2018-01-03 14:27:17 +0530

    Linting with shellcheck v0.4.6

--- bin/crlf.sh
+++ bin/crlf.sh
@@ -5,7 +5,7 @@
 # Usage:
 #   crlf [file] [--force]
 
-local force=
+force=
 
 function _crlf_file() {
   grep -q $'\x0D' "$1" && echo "$1" && [ "$2" ] && dos2unix "$1"
@@ -20,6 +20,6 @@ fi
 
 # All files
 [ "$1" == "--force" ] && force=1 || force=0
-for file in $(find . -type f -not -path "*/.git/*" -not -path "*/node_modules/*" | xargs file | grep ASCII | cut -d: -f1); do
+for file in $(find . -type f -not -path "*/.git/*" -not -path "*/node_modules/*" -print0 | xargs -0 file | grep ASCII | cut -d: -f1); do
   _crlf_file "$file" $force
 done
--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -175,9 +175,9 @@ brew install https://raw.github.com/gleitz/howdoi/master/howdoi.rb
 
 install_oh_my_zsh () {
     # Test to see if zshell is installed.  If it is:
-    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+    if [ -f /bin/zsh ] || [ -f /usr/bin/zsh ]; then
         # Install Oh My Zsh if it isn't already present
-        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+        if [[ ! -d $HOME/oh-my-zsh/ ]]; then
             sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
         fi
         # Set the default shell to zsh if it isn't currently set to zsh
