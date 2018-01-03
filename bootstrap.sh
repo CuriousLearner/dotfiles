42279ba5686d1c4b51b4cd447ecef87ebc97a8df
storymode7 <17mayanksinghal@gmail.com>
2018-01-04 01:11:52 +0530

    Removing unnecessary comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,10 +23,6 @@ function echoB() {
 
 # Get file list
 function getFilesInDir() {
-  #  ls -lah | awk '{
-  #      if ($9 != "" && $9 != "." && $9 != ".." && $9 != ".git" && $9 != ".DS_Store" && $9 != ".gitmodules" && $9 != "README.md" && $9 != "bootstrap.sh"  && $9 != "setup" && $9 != ".private" && $9 != ".ssh" && $9 != "bin")
-  #          print $9
-  #      }'
     find . -maxdepth 1 -exec awk '{
         if ($9 != "" && $9 != "." && $9 != ".." && $9 != ".git" && $9 != ".DS_Store" && $9 != ".gitmodules" && $9 != "README.md" && $9 != "bootstrap.sh"  && $9 != "setup" && $9 != ".private" && $9 != ".ssh" && $9 != "bin")
             print $9
