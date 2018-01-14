b05df784633e4d3c71aba55fe88cc6c1da91cf5a
Sanyam Khurana <8039608+CuriousLearner@users.noreply.github.com>
2018-01-15 00:25:35 +0530

    feat(*): Add shell check and fedora_setup script (#3)
    * feat(tests): Add tests via shellcheck
    
    * feat(setup): Adds setup file for fedora (#2)
    
    * First draft of Fedora setup
    
    * Adding howdoi
    
    * Added binary installation
    
    * Fixed one command
    
    * Fixing minor error
    
    * Some improvements
    
    * Improving comments
    
    * Adding ShellCheck and teamviewer
    
    * Updating with ShellCheck recommendations
    
    * fix(shell scripts): correctly linted using shellcheck
    
    * Linting using ShellCheck v0.4.6
    
    * Linting with shellcheck v0.4.6
    
    * Adding Fedora Setup
    
    Linting the commands it has, also improving the comments plus adding
    some more packages.
    
    * Removing unnecessary comments
    
    * Added slack and fixed comment spacing
    
    * Fixing merge conflict in fedora setup
    
    * Adding .travis.yml
    
    * fix(shell_script): fixes shellcheck errors SC1117
    
    * Update README.md with Travis Badge
    
    * Correcting branch and repo

--- bin/crlf.sh
+++ bin/crlf.sh
@@ -5,21 +5,21 @@
 # Usage:
 #   crlf [file] [--force]
 
-local force=
+force=
 
 function _crlf_file() {
-  grep -q $'\x0D' "$1" && echo "$1" && [ $2 ] && dos2unix "$1"
+  grep -q $'\x0D' "$1" && echo "$1" && [ "$2" ] && dos2unix "$1"
 }
 
 # Single file
 if [ "$1" != "" ] && [ "$1" != "--force" ]; then
   [ "$2" == "--force" ] && force=1 || force=0
-  _crlf_file $1 $force
+  _crlf_file "$1" $force
   return
 fi
 
 # All files
 [ "$1" == "--force" ] && force=1 || force=0
-for file in $(find . -type f -not -path "*/.git/*" -not -path "*/node_modules/*" | xargs file | grep ASCII | cut -d: -f1); do
-  _crlf_file $file $force
+for file in $(find . -type f -not -path "*/.git/*" -not -path "*/node_modules/*" -print0 | xargs -0 file | grep ASCII | cut -d: -f1); do
+  _crlf_file "$file" $force
 done
