ee32164e5ba21d77e80c4d1fa2a3814e35a0a677
Shiva Saxena <shivasaxena911@gmail.com>
2018-01-16 21:07:14 +0530

    fix(tor.sh): fix lint errors (#4)

--- bin/tor.sh
+++ bin/tor.sh
@@ -12,9 +12,9 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # trap ctrl-c and call disable_proxy()
 function disable_proxy() {
     sudo networksetup -setsocksfirewallproxystate $INTERFACE off
-    echo "$(tput setaf 64)" #green
+    tput setaf 64 #green
     echo "SOCKS proxy disabled."
-    echo "$(tput sgr0)" # color reset
+    tput sgr0 # color reset
 }
 trap disable_proxy INT
 
@@ -22,10 +22,10 @@ trap disable_proxy INT
 sudo networksetup -setsocksfirewallproxy $INTERFACE 127.0.0.1 9050 off
 sudo networksetup -setsocksfirewallproxystate $INTERFACE on
 
-echo "$(tput setaf 64)" # green
+tput setaf 64 # green
 echo "SOCKS proxy 127.0.0.1:9050 enabled."
-echo "$(tput setaf 136)" # orange
+tput setaf 136 # orange
 echo "Starting Tor..."
-echo "$(tput sgr0)" # color reset
+tput sgr0 # color reset
 
 tor
