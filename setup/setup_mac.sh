03bc1d1d87028bb1d38a7d522b3bde489de0e095
Sanyam Khurana <sanyam.khurana01@gmail.com>
2019-01-02 00:17:37 +0530

    feat(setup): Add terraform

--- setup/setup_mac.sh
+++ setup/setup_mac.sh
@@ -82,6 +82,7 @@ brew install hg         # Mercurial for FOSS projects (mainly Mozilla)
 brew install latex2html
 brew install pre-commit  # https://pre-commit.com/
 brew install libmemcached  # for Django Developement
+brew install terrraform
 
 # Native apps
 brew tap phinze/homebrew-cask
