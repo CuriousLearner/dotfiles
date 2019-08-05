5a64135f6f7ece9d45b1fcc2cdd0152c17363fb7
Sanyam Khurana <sanyam.khurana01@gmail.com>
2019-08-06 00:22:46 +0530

    fix(bootstrap.sh): revert linting changes

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,30 +4,29 @@
 # Colors
 function echoY() {
     prompt="$1"
-    echo -e -n "\\033[32m$prompt"
+    echo -e -n "\033[32m$prompt"
     echo -e -n '\033[0m'
     echo ''
 }
 function echoR() {
     prompt="$1"
-    echo -e -n "\\033[31m$prompt"
+    echo -e -n "\033[31m$prompt"
     echo -e -n '\033[0m'
     echo ''
 }
 function echoB() {
     prompt="$1"
-    echo -e -n "\\033[34m$prompt"
+    echo -e -n "\033[34m$prompt"
     echo -e -n '\033[0m'
     echo ''
 }
 
 # Get file list
 function getFilesInDir() {
-    find . -maxdepth 1 -exec awk '{
+    ls -lah | awk '{
         if ($9 != "" && $9 != "." && $9 != ".." && $9 != ".git" && $9 != ".DS_Store" && $9 != ".gitmodules" && $9 != "README.md" && $9 != "bootstrap.sh"  && $9 != "setup" && $9 != ".private" && $9 != ".ssh" && $9 != "bin")
             print $9
-        }' '{}' ';'
- 
+        }'
 }
 
 # Set vars
@@ -41,17 +40,17 @@ if [ "$1" == "--force" ]; then
 fi
 
 function createSymlinks() {
-    for F in "${FILES[@]}"; do
+    for F in ${FILES[@]}; do
         # Delete files if --force was used
         if [ $FORCE == true ]; then
-            TIME=$(date +%s)
+            TIME= `date +%s`
             echoR "--> [BACKUP]: $HOME/${F}"
-            mv "$HOME"/"$F" "$HOME"/"$F"."$TIME".bak
+            mv $HOME/$F $HOME/$F.$TIME.bak
         fi
 
         # Make symlink
         echoY "--> [LINK]: ${HOME}/${F} -> ${CURRENTPATH}/${F}"
-        ln -s "$CURRENTPATH"/"$F" "$HOME"/"$F"
+        ln -s $CURRENTPATH/$F $HOME/$F
 
         if [ $? -eq 1 ]; then
             echo
@@ -71,11 +70,11 @@ createSymlinks
 ## Install custom binary utilities
 
 if [ ! -d "$HOME/.bin" ]; then
-  mkdir "$HOME"/.bin
+  mkdir $HOME/.bin
 fi
 
 # Symlink binaries
-ln -sf "$PWD"/bin/* "$HOME"/.bin/
+ln -sf $PWD/bin/* $HOME/.bin/
 
 declare -a BINARIES=(
     'batcharge.py'
@@ -96,9 +95,9 @@ declare -a BINARIES=(
     'tweet'
 )
 
-for i in "${BINARIES[@]}"; do
+for i in ${BINARIES[@]}; do
     echo "Changing access permissions for binary script :: ${i##*/}"
-    chmod +rwx "$HOME"/.bin/"${i##*/}"
+    chmod +rwx $HOME/.bin/${i##*/}
 done
 
 echo "Binaries installed"
@@ -114,5 +113,5 @@ unset getFilesInDir
 unset createSymlinks
 unset BINARIES
 
-# shellcheck disable=SC1090
 source ~/.zshrc
+
