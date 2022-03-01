a45d934e6c225376949b9568b8a13714c0716ff4
Sanyam Khurana <sanyam.khurana01@gmail.com>
2022-03-01 16:28:34 +0530

    eve - Update VS-Code extensions + config

--- setup/vscode/install-extensions.sh
+++ setup/vscode/install-extensions.sh
@@ -1,9 +1,18 @@
+code --install-extension budparr.language-hugo-vscode --force &
+code --install-extension dsznajder.es7-react-js-snippets --force &
 code --install-extension eamodio.gitlens --force &
+code --install-extension esbenp.prettier-vscode --force &
+code --install-extension gitkraken.gitkraken-authentication --force &
+code --install-extension hashicorp.terraform --force &
 code --install-extension ms-python.python --force &
 code --install-extension ms-python.vscode-pylance --force &
 code --install-extension ms-toolsai.jupyter --force &
 code --install-extension ms-toolsai.jupyter-keymap --force &
 code --install-extension ms-toolsai.jupyter-renderers --force &
 code --install-extension ms-vscode.sublime-keybindings --force &
+code --install-extension oliversturm.fix-json --force &
 code --install-extension rickynormandeau.mariana-pro --force &
-code --install-extension wholroyd.jinja --force
+code --install-extension tamasfe.even-better-toml --force &
+code --install-extension tomoki1207.pdf --force &
+code --install-extension wholroyd.jinja --force &
+code --install-extension yzhang.markdown-all-in-one --force
